0ab6eaed305c041254650eb9b6e360cdbb87cd05
Abel Salgado Romero <abelromero@gmail.com>
2019-04-02 23:42:51 +0200

    fix detect speakers by name

--- src/index.ts
+++ src/index.ts
@@ -7,33 +7,34 @@ const password = '-'
 new ApiConnector('https://cfpapi.jbcnconf.com')
     .getToken(user, password, api => {
 
-        console.log(`NAME;EMAIL;PAPER_TITLE;AVG_VOTE`)
-
+        console.log(`NAME;EMAIL;PAPER_TITLE;AVG_VOTE;VOTES`)
         api.getTalks(talks => {
 
-            let acceptedSpeakers = new Map<String, Sender>()
-            let acceptedSpeakersMails = new Array<String>()
+            let acceptedSpeakers = new Array<Sender>()
 
-            talks.forEach(t => {
-                t.speakers.forEach(speaker => {
-                    acceptedSpeakers.set(speaker.email, speaker)
-                    acceptedSpeakersMails.push(speaker.email)
+            talks.forEach(talk => {
+                talk.speakers.forEach(speaker => {
+                    acceptedSpeakers.push(speaker)
                 });
             })
 
             api.getPapers(papers => {
-                papers.forEach(p => {
-                    let notAcceptedSpeakers = new Array<Sender>()
-                    p.senders.forEach(s => {
-                        if (!acceptedSpeakersMails.includes(s.email)) {
-                            console.log(`${s.fullName};${s.email};"${p.title}";${p.averageVote}`)
-                            notAcceptedSpeakers.push(s)
+                let acceptedEmails = acceptedSpeakers.map(s => s.email.trim()).sort()
+                let acceptedNames = acceptedSpeakers.map(s => s.fullName.trim()).sort()
+                let notAcceptedSpeakers = new Map<String, Sender>()
+                papers.forEach(paper => {
+
+                    paper.senders.forEach(s => {
+                        // beware some speakers registered with different emails
+                        if (!acceptedEmails.includes(s.email.trim()) && !acceptedNames.includes(s.fullName.trim())) {
+                            notAcceptedSpeakers.set(s.fullName.trim(), s)
+                            console.log(`${s.fullName.trim()};${s.email.trim()};"${paper.title}";${paper.averageVote};${paper.votes ? paper.votes.length : 0}`)
                         }
                     })
                     /*
-                    notAcceptedSpeakers
+                    Array.from(notAcceptedSpeakers.values())
                         .forEach(sender => {
-                            console.log(`${sender.fullName};${sender.email};"${paper.title}";${paper.averageVote}`)
+                            console.log(`${sender.fullName.trim()};${sender.email.trim()};"${paper.title}";${paper.averageVote};${paper.votes ? paper.votes.length : 0}`)
                         })
                     */
                 })
